98049c6a580f7234ac8d313afddfd5bcb33c0f4a
adarshh347 <adarsh347k@gmail.com>
2025-11-07 16:47:47 +0530

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,5 +7,14 @@ def create_app() -> FastAPI:
 	app.add_middleware(
 		CORSMiddleware,
 		allow_origins=["*"],
-
+		allow_credentials=True,
+		allow_methods=["*"],
+		allow_headers=["*"],
 	)
+	app.include_router(chat_router, prefix="/api")
+	@app.get("/", tags=["Root"])
+	async def read_root():
+		return {"message":"Welcome to fashion assistant API!"}
+	return app
+
+app=create_app()
